d9e4977febe1bd22f7918ad52c8ab2601823f3f5
mostafabander <mostafabander01@gmail.com>
2022-12-29 20:25:18 -0500

    added functions for task manipulation

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,30 +1,30 @@
-import PropTypes from 'prop-types'
-import Button from './Button'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log('Click')
-  }
-  
+const Header = ({ title, onAdd, showAdd }) => {
   return (
-    <header className='header'>
+    <header className="header">
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick}/>
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
-  )
-}
+  );
+};
 
 Header.defaultProps = {
-  title: 'Task Tracker'
-}
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
-}
+};
 // CSS in JS
 // const headingStyle = {
-//     color: 'red', 
+//     color: 'red',
 //     backgroundColor: 'black'
 //   }
 
-export default Header
+export default Header;
